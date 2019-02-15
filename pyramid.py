9393a61baced9d8afed53808ebc312bf81f2d48a
Mike Starbuck <miglas9@gmail.com>
2019-02-15 16:29:36 -0600

    added pyramid.py

--- pyramid.py
+++ pyramid.py
@@ -1,1 +1,11 @@
-# TO DO
+# this makes a pyramid
+def make_a_pyramid():
+    spaces = 8  # counts spaces to print
+    stars = 1  # counts * to print
+    while spaces >= 0:
+        print(" " * spaces + "*" * stars)
+        spaces -= 1
+        stars += 2
+
+
+make_a_pyramid()
